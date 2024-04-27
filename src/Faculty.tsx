29146fc38e97dd1cdf5dd88f6189a665eb4cc8d0
avaashh <avaashbhattarai@gmail.com>
2024-04-27 14:44:26 -0500

    champion steward flip and clean

--- src/Faculty.tsx
+++ src/Faculty.tsx
@@ -1,62 +1,18 @@
-import React from "react";
 import Grid from "@mui/material/Grid";
 
-import PieChartNivo from "./charts/PieChartNivo";
 import BubbleChartNivo from "./charts/BubbleChartNivo";
 
-import { stewardsBubble, stewardsPie } from "./data/faculty/stewards";
-import { championPie, championBubble } from "./data/faculty/champions";
-import { assetPie, assetBubble } from "./data/faculty/assets";
-import { desiredPie, plentifulPie } from "./data/faculty/plentifuldesired";
-import { gapsPie } from "./data/faculty/gaps";
-import ThemesList, { Theme } from "./components/ThemesList";
-import { gapThemesData } from "./data/faculty/gapThemes";
-import { desiredThemesData } from "./data/faculty/desiredThemes";
+import { stewardsBubble } from "./data/faculty/stewards";
+import { championBubble } from "./data/faculty/champions";
+import { assetBubble } from "./data/faculty/assets";
 import Heading from "./components/Heading";
 
 const FacultyPage = () => {
-  const [gapThemes, setGapThemes] = React.useState<Theme[]>([]);
-  const [desThemes, setDesThemes] = React.useState<Theme[]>([]);
-
-  const handleThemeClick = (
-    selectedId: string,
-    all_themes: any,
-    themes: Theme[],
-    setter: (_: Theme[]) => void
-  ) => {
-    let themeSelected = false;
-    for (let i = 0; i < themes.length && !themeSelected; i++)
-      themeSelected = selectedId === themes[i]._id;
-
-    if (themeSelected) {
-      const newThemes = themes.filter((t) => t._id !== selectedId);
-      setter(newThemes);
-    } else {
-      const newThemes = [
-        ...themes,
-        { _id: selectedId, v: all_themes[selectedId] },
-      ];
-      setter(newThemes);
-    }
-  };
-
-  const gapThemehandler = (k: string) =>
-    handleThemeClick(k, gapThemesData, gapThemes, setGapThemes);
-
-  const desThemehandler = (k: string) =>
-    handleThemeClick(k, desiredThemesData, desThemes, setDesThemes);
-
   return (
     <Grid container spacing={1} style={{ height: "100%" }}>
       <Grid item xs={12}>
         <Heading text={"Faculty Advising Input"} />
       </Grid>
-      {/* <Grid item xs={6} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Assets (as Identified by Faculty)"}
-          data={assetPie}
-        />
-      </Grid> */}
       <Grid
         item
         xs={12}
@@ -68,29 +24,6 @@ const FacultyPage = () => {
           data={assetBubble}
         />
       </Grid>
-      {/* <Grid item xs={6} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Stewards (as Identified by Faculty)"}
-          data={stewardsPie}
-        />
-      </Grid> */}
-      <Grid
-        item
-        xs={12}
-        className="grid-container"
-        style={{ minHeight: "100vh" }}
-      >
-        <BubbleChartNivo
-          title={"Stewards (as Identified by Faculty)"}
-          data={stewardsBubble}
-        />
-      </Grid>
-      {/* <Grid item xs={6} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Champion (as Identified by Faculty)"}
-          data={championPie}
-        />
-      </Grid> */}
       <Grid
         item
         xs={12}
@@ -102,33 +35,17 @@ const FacultyPage = () => {
           data={championBubble}
         />
       </Grid>
-
-      {/* <Grid item xs={4} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Resources Seen as Plentiful"}
-          data={plentifulPie}
+      <Grid
+        item
+        xs={12}
+        className="grid-container"
+        style={{ minHeight: "100vh" }}
+      >
+        <BubbleChartNivo
+          title={"Stewards (as Identified by Faculty)"}
+          data={stewardsBubble}
         />
       </Grid>
-      <Grid item xs={4} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Desired Resources"}
-          data={desiredPie}
-          onClick={desThemehandler}
-        />
-      </Grid>
-      <Grid item xs={4} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Gaps Noted (categories AI generated)"}
-          data={gapsPie}
-          onClick={gapThemehandler}
-        />
-      </Grid>
-      <Grid item xs={6} className="grid-container">
-        <ThemesList title={"Desired Themes"} themes={desThemes} />
-      </Grid>
-      <Grid item xs={6} className="grid-container">
-        <ThemesList title={"Gap Themes"} themes={gapThemes} />
-      </Grid> */}
     </Grid>
   );
 };
--- src/Staff.tsx
+++ src/Staff.tsx
@@ -1,62 +1,18 @@
-import React from "react";
 import Grid from "@mui/material/Grid";
 
-import PieChartNivo from "./charts/PieChartNivo";
 import BubbleChartNivo from "./charts/BubbleChartNivo";
 
-import { stewardsBubble, stewardsPie } from "./data/staff/stewards";
-import { championPie, championBubble } from "./data/staff/champions";
-import { assetPie, assetBubble } from "./data/staff/assets";
-import { desiredPie, plentifulPie } from "./data/staff/plentifuldesired";
-import { gapsPie } from "./data/staff/gaps";
-import ThemesList, { Theme } from "./components/ThemesList";
-import { gapThemesData } from "./data/staff/gapThemes";
-import { desiredThemesData } from "./data/staff/desiredThemes";
+import { stewardsBubble } from "./data/staff/stewards";
+import { championBubble } from "./data/staff/champions";
+import { assetBubble } from "./data/staff/assets";
 import Heading from "./components/Heading";
 
 const StaffPage = () => {
-  const [gapThemes, setGapThemes] = React.useState<Theme[]>([]);
-  const [desThemes, setDesThemes] = React.useState<Theme[]>([]);
-
-  const handleThemeClick = (
-    selectedId: string,
-    all_themes: any,
-    themes: Theme[],
-    setter: (_: Theme[]) => void
-  ) => {
-    let themeSelected = false;
-    for (let i = 0; i < themes.length && !themeSelected; i++)
-      themeSelected = selectedId === themes[i]._id;
-
-    if (themeSelected) {
-      const newThemes = themes.filter((t) => t._id !== selectedId);
-      setter(newThemes);
-    } else {
-      const newThemes = [
-        ...themes,
-        { _id: selectedId, v: all_themes[selectedId] },
-      ];
-      setter(newThemes);
-    }
-  };
-
-  const gapThemehandler = (k: string) =>
-    handleThemeClick(k, gapThemesData, gapThemes, setGapThemes);
-
-  const desThemehandler = (k: string) =>
-    handleThemeClick(k, desiredThemesData, desThemes, setDesThemes);
-
   return (
     <Grid container spacing={1} style={{ height: "100%" }}>
       <Grid item xs={12}>
         <Heading text={"Staff Advising Input"} />
       </Grid>
-      {/* <Grid item xs={6} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Assets (as Identified by Staff)"}
-          data={assetPie}
-        />
-      </Grid> */}
       <Grid
         item
         xs={12}
@@ -68,29 +24,6 @@ const StaffPage = () => {
           data={assetBubble}
         />
       </Grid>
-      {/* <Grid item xs={6} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Stewards (as Identified by Staff)"}
-          data={stewardsPie}
-        />
-      </Grid> */}
-      <Grid
-        item
-        xs={12}
-        className="grid-container"
-        style={{ minHeight: "100vh" }}
-      >
-        <BubbleChartNivo
-          title={"Stewards (as Identified by Staff)"}
-          data={stewardsBubble}
-        />
-      </Grid>
-      {/* <Grid item xs={6} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Champion (as Identified by Staff)"}
-          data={championPie}
-        />
-      </Grid> */}
       <Grid
         item
         xs={12}
@@ -102,34 +35,17 @@ const StaffPage = () => {
           data={championBubble}
         />
       </Grid>
-      {/* // */}
-      {/* <Grid item xs={4} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Resources Seen as Plentiful"}
-          data={plentifulPie}
+      <Grid
+        item
+        xs={12}
+        className="grid-container"
+        style={{ minHeight: "100vh" }}
+      >
+        <BubbleChartNivo
+          title={"Stewards (as Identified by Staff)"}
+          data={stewardsBubble}
         />
       </Grid>
-      <Grid item xs={4} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Desired Resources"}
-          data={desiredPie}
-          onClick={desThemehandler}
-        />
-      </Grid>
-      <Grid item xs={4} className="grid-container" style={{ minHeight: 600 }}>
-        <PieChartNivo
-          title={"Gaps Noted"}
-          data={gapsPie}
-          onClick={gapThemehandler}
-        />
-      </Grid> */}
-      {/* // */}
-      {/* <Grid item xs={6} className="grid-container">
-        <ThemesList title={"Desired Themes"} themes={desThemes} />
-      </Grid>
-      <Grid item xs={6} className="grid-container">
-        <ThemesList title={"Gap Themes"} themes={gapThemes} />
-      </Grid> */}
     </Grid>
   );
 };
