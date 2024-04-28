444ec375e2754ea2a05b2096748b13bf5c82cada
Jiahao-Grinnell <142425824+Jiahao-Grinnell@users.noreply.github.com>
2024-04-28 17:23:05 -0500

    Update onclickevent.ts

--- src/data/home/onclickevent.ts
+++ src/data/home/onclickevent.ts
@@ -25,30 +25,29 @@ export const onclickdefs = {
   },
 
   fs: {
-    "Academic Advising":
-      "Academic Support & Resources\nChoosing a Major/Concentration\nFinding Balance/Personal Needs\nProfessors and Classes\n4-Year Planning\nCareer Planning\nCareer Skills (How do I …)\nCourse Selection & Registration\nGraduate/Professional School\nIdentity-Specific Needs\nInternships/Gaining Experience\nMAPs & Independent Study\nOCS & Co-Curriculars",
-    "Academic Resources":
-      "Academic Support & Resources\nCareer Planning\nCareer Skills (How do I …)\nCourse Selection & Registration\nProfessors and Classes\nChoosing a Major/Concentration\nGraduate/Professional School\n4-Year Planning\nInternships/Gaining Experience\nOCS & Co-Curriculars\nMAPs & Independent Study\nFinding Balance/Personal Needs\nIdentity-Specific Needs",
-    "Accessibility & Disability Resources":
-      "Academic Support & Resources\nCourse Selection & Registration\nGraduate/Professional School\nIdentity-Specific Needs\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes\n4-Year Planning\nCareer Planning\nCareer Skills (How do I …)\nChoosing a Major/Concentration\nFinding Balance/Personal Needs\nMAPs & Independent Study",
-    "Admission, Financial Aid, & Student Accounts":
-      "OCS & Co-Curriculars\nProfessors and Classes\n4-Year Planning\nFinding Balance/Personal Needs\nAcademic Support & Resources\nCareer Planning\nCareer Skills (How do I …)\nChoosing a Major/Concentration\nCourse Selection & Registration\nGraduate/Professional School\nIdentity-Specific Needs\nInternships/Gaining Experience\nMAPs & Independent Study",
-    "Advising Entities": "Types of Advising (Faculty & Staff)",
-    CLS: "OCS & Co-Curriculars\nCareer Skills (How do I …)\nCareer Planning\nChoosing a Major/Concentration\nIdentity-Specific Needs\nInternships/Gaining Experience\nProfessors and Classes\n4-Year Planning\nFinding Balance/Personal Needs\nMAPs & Independent Study\nAcademic Support & Resources\nCourse Selection & Registration\nGraduate/Professional School",
-    CRSSJ:
-      "Academic Support & Resources\nFinding Balance/Personal Needs\nIdentity-Specific Needs\nProfessors and Classes\n4-Year Planning\nCareer Planning\nCareer Skills (How do I …)\nChoosing a Major/Concentration\nCourse Selection & Registration\nGraduate/Professional School\nInternships/Gaining Experience\nMAPs & Independent Study\nOCS & Co-Curriculars",
-    "Coaches & PEAR Staff":
-      "Academic Support & Resources\nChoosing a Major/Concentration\nCourse Selection & Registration\nFinding Balance/Personal Needs\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes\n4-Year Planning\nCareer Planning\nCareer Skills (How do I …)\nGraduate/Professional School\nIdentity-Specific Needs\nMAPs & Independent Study",
-    "Course Instructors & Faculty Mentors":
-      "Career Planning\nProfessors and Classes\nGraduate/Professional School\nAcademic Support & Resources\nIdentity-Specific Needs\nChoosing a Major/Concentration\nInternships/Gaining Experience\nMAPs & Independent Study\nCourse Selection & Registration\nCareer Skills (How do I …)\nFinding Balance/Personal Needs\nOCS & Co-Curriculars\n4-Year Planning",
-    "Faculty Advisor":
-      "4-Year Planning\nCourse Selection & Registration\nProfessors and Classes\nCareer Planning\nChoosing a Major/Concentration\nOCS & Co-Curriculars\nAcademic Support & Resources\nGraduate/Professional School\nIdentity-Specific Needs\nInternships/Gaining Experience\nFinding Balance/Personal Needs\nMAPs & Independent Study\nCareer Skills (How do I …)",
-    IGE: "Academic Support & Resources\nCourse Selection & Registration\nGraduate/Professional School\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes\n4-Year Planning\nCareer Planning\nCareer Skills (How do I …)\nChoosing a Major/Concentration\nFinding Balance/Personal Needs\nIdentity-Specific Needs\nMAPs & Independent Study",
-    ODEI: "Academic Support & Resources\nFinding Balance/Personal Needs\nIdentity-Specific Needs\nOCS & Co-Curriculars\n4-Year Planning\nCareer Planning\nCareer Skills (How do I …)\nChoosing a Major/Concentration\nCourse Selection & Registration\nGraduate/Professional School\nInternships/Gaining Experience\nMAPs & Independent Study\nProfessors and Classes",
-    OISA: "Academic Support & Resources\nCareer Planning\nChoosing a Major/Concentration\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes\n4-Year Planning\nCareer Skills (How do I …)\nCourse Selection & Registration\nFinding Balance/Personal Needs\nGraduate/Professional School\nIdentity-Specific Needs\nMAPs & Independent Study",
-    "Registrar's Office":
-      "4-Year Planning\nAcademic Support & Resources\nChoosing a Major/Concentration\nCourse Selection & Registration\nMAPs & Independent Study\nOCS & Co-Curriculars\nProfessors and Classes\nCareer Planning\nCareer Skills (How do I …)\nFinding Balance/Personal Needs\nGraduate/Professional School\nIdentity-Specific Needs\nInternships/Gaining Experience",
-    Supervisors:
-      "Graduate/Professional School\n4-Year Planning\nAcademic Support & Resources\nCareer Planning\nCareer Skills (How do I …)\nChoosing a Major/Concentration\nCourse Selection & Registration\nFinding Balance/Personal Needs\nIdentity-Specific Needs\nInternships/Gaining Experience\nMAPs & Independent Study\nOCS & Co-Curriculars\nProfessors and Classes",
+   "Academic Advising": "Academic Support & Resources\nChoosing a Major/Concentration\nFinding Balance/Personal Needs\nProfessors and Classes",
+  "Academic Department Leadership": "(Did not survey)",
+  "Academic Resources": "Academic Support & Resources\nCareer Planning\nCareer Skills (How do I …)\nCourse Selection & Registration\nProfessors and Classes\nChoosing a Major/Concentration\nGraduate/Professional School\n4-Year Planning\nInternships/Gaining Experience\nOCS & Co-Curriculars\nMAPs & Independent Study",
+  "Accessibility & Disability Resources": "Academic Support & Resources\nCourse Selection & Registration\nGraduate/Professional School\nIdentity-Specific Needs\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes",
+  "Admission, Financial Aid, & Student Accounts": "OCS & Co-Curriculars\nProfessors and Classes\n4-Year Planning\nFinding Balance/Personal Needs",
+  "Board of Trustees": "(Did not survey)",
+  "CLS": "OCS & Co-Curriculars\nCareer Skills (How do I …)\nCareer Planning\nChoosing a Major/Concentration\nIdentity-Specific Needs\nInternships/Gaining Experience\nProfessors and Classes\n4-Year Planning\nFinding Balance/Personal Needs\nMAPs & Independent Study",
+  "CRSSJ": "Academic Support & Resources\nFinding Balance/Personal Needs\nIdentity-Specific Needs\nProfessors and Classes",
+  "Coaches & PEAR Staff": "Academic Support & Resources\nChoosing a Major/Concentration\nCourse Selection & Registration\nFinding Balance/Personal Needs\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes",
+  "Course Instructors & Faculty Mentors": "Career Planning\nProfessors and Classes\nGraduate/Professional School\nAcademic Support & Resources\nIdentity-Specific Needs\nChoosing a Major/Concentration\nInternships/Gaining Experience\nMAPs & Independent Study\nCourse Selection & Registration\nCareer Skills (How do I …)\nFinding Balance/Personal Needs\nOCS & Co-Curriculars\n4-Year Planning",
+  "DSA": "Finding Balance/Personal Needs\nCareer Skills (How do I …)\nOCS & Co-Curriculars\nProfessors and Classes",
+  "Faculty Adviser": "4-Year Planning\nCourse Selection & Registration\nProfessors and Classes\nCareer Planning\nChoosing a Major/Concentration\nOCS & Co-Curriculars\nAcademic Support & Resources\nGraduate/Professional School\nIdentity-Specific Needs\nInternships/Gaining Experience\nFinding Balance/Personal Needs\nMAPs & Independent Study\nCareer Skills (How do I …)",
+  "Family": "(Did not survey)",
+  "Healthcare Professionals (SHAW)": "Finding Balance/Personal Needs\nProfessors and Classes\nCareer Planning",
+  "IGE": "Academic Support & Resources\nCourse Selection & Registration\nGraduate/Professional School\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes",
+  "Identity Groups": "(Did not survey)",
+  "ODEI": "Academic Support & Resources\nFinding Balance/Personal Needs\nIdentity-Specific Needs\nOCS & Co-Curriculars",
+  "OISA": "Academic Support & Resources\nCareer Planning\nChoosing a Major/Concentration\nInternships/Gaining Experience\nOCS & Co-Curriculars\nProfessors and Classes",
+  "Ombuds": "(Did not survey)",
+  "Online Resources": "(N/A)",
+  "Registrar's Office": "4-Year Planning\nAcademic Support & Resources\nChoosing a Major/Concentration\nCourse Selection & Registration\nMAPs & Independent Study\nOCS & Co-Curriculars\nProfessors and Classes",
+  "Student Leaders": "(Did not survey)",
+  "Students, Alumni, & Friends": "(Did not survey)",
+  "Supervisors": "Graduate/Professional School",
   },
 };
